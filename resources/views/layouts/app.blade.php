b6430d21b1984dcc090ec556516f5f301cbab7d0
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-08-23 14:53:06 -0500

    guest mode link remove

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,10 @@
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                    @if (Auth::check())
                     <a class="no-underline hover:underline" href="/inventory">Manage Inventory</a>
                     <a class="no-underline hover:underline" href="/users">Manage Users</a>
+                    @endif
                     @guest
                     <a class="no-underline hover:underline" href="{{ route('login') }}"><button class="bg-white hover:bg-blue-500 text-gray-900 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</button></a>
                     @if (Route::has('register'))
